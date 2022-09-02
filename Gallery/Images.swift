1567f6f4a84e2f5d5aca80f08c097f1f6bb90310
KevinH1994 <kevinhering2@web.de>
2022-09-02 15:54:34 +0200

    Noch nicht ganz fertig :)

---
+++ Gallery/Images.swift
@@ -0,0 +1,25 @@
+//
+//  Images.swift
+//  Gallery
+//
+//  Created by Kevin Hering on 02.09.22.
+//
+
+import Foundation
+import UIKit
+
+struct Image{
+    static let Mona_Lisa: UIImage = UIImage(named: "Mona_Lisa.jpeg")!
+    static let Der_Schrei: UIImage = UIImage(named: "Der_Schrei.jpeg")!
+    static let Bild1 : UIImage = UIImage (named: "Bild1.jpeg")!
+    static let Bild2 : UIImage = UIImage (named: "Bild2.jpeg")!
+    static let Bild3 : UIImage = UIImage (named: "Bild3.jpeg")!
+    static let Bild4 : UIImage = UIImage (named: "Bild4.jpeg")!
+    static let Bild5 : UIImage = UIImage (named: "Bild5.jpeg")!
+    static let Bild6 : UIImage = UIImage (named: "Bild6.jpeg")!
+    static let Bild7 : UIImage = UIImage (named: "Bild7.jpeg")!
+    static let Bild8 : UIImage = UIImage (named: "Bild8.jpeg")!
+    static let Bild9 : UIImage = UIImage (named: "Bild9.jpeg")!
+    static let Bild10 : UIImage = UIImage (named: "Bild10.jpeg")!
+    
+}
--- Gallery/ViewController.swift
+++ Gallery/ViewController.swift
@@ -7,13 +7,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct MyCustomData {
+    var image = UIImage()
 }
 
+class ViewController: UIViewController {
+    
+    fileprivate let data =  [
+    
+        MyCustomData(image: Image.Mona_Lisa),
+        MyCustomData (image: Image.Der_Schrei),
+        MyCustomData(image: Image.Bild1),
+        MyCustomData(image: Image.Bild2),
+        MyCustomData(image: Image.Bild3),
+        MyCustomData(image: Image.Bild4),
+        MyCustomData(image: Image.Bild5),
+        MyCustomData(image: Image.Bild6),
+        MyCustomData(image: Image.Bild7),
+        MyCustomData(image: Image.Bild8),
+        MyCustomData(image: Image.Bild9),
+        MyCustomData(image: Image.Bild10)
+
+    ]
+
+    fileprivate let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(MyCustomCell.self, forCellWithReuseIdentifier: "cell")
+        cv.backgroundColor = .white
+        return cv
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Gallery"
+        
+        setupView()
+        setupConstraint()
+    }
+    
+    func setupView(){
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    func setupConstraint() {
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    
+}
+extension ViewController: UICloudSharingControllerDelegate, UICollectionViewDataSource,
+                          UICollectionViewDelegateFlowLayout {
+    func cloudSharingController(_ csc: UICloudSharingController, failedToSaveShareWithError error: Error) {
+    
+    }
+    
+    func itemTitle(for csc: UICloudSharingController) -> String? {
+        return ""
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCustomCell
+        //cell.backgroundColor = .blue
+        
+        cell.data = data[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int ) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView : UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexpath: IndexPath) -> CGSize {
+        var Width: CGFloat = collectionView.frame.width/4 - 1
+        
+        if UIDevice.current.orientation.isLandscape {
+            Width = collectionView.frame.width/6 - 1
+        }
+        
+        return CGSize (width: Width, height: Width)
+    }
+}
